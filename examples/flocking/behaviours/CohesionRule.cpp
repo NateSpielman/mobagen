728aaf1724b6b6495dcf00332d204694a1b6d604
nate spielman <nate.spielman@mymail.champlain.edu>
2024-08-31 16:17:04 -0400

    Cohesion rule mostly implemented, needs small bug fix to stop boids from circling alone.

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -6,8 +6,22 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
 
   // todo: add your code here to make a force towards the center of mass
   // hint: iterate over the neighborhood
+  if (!neighborhood.empty()) {
+    Vector2f position = boid->transform.position;
+    Vector2f centerOfMass;
+    float countFlockmates = 0;
+    for (int i = 0; i < neighborhood.size(); i++) {
+      Boid* neighbor = neighborhood[i];
+      Vector2f neighborPosition = neighbor->transform.position;
+      centerOfMass = centerOfMass + neighborPosition;
+      countFlockmates++;
+    }
+    // find center of mass
+    centerOfMass = centerOfMass / countFlockmates;
+    cohesionForce = centerOfMass - position;
+  }
 
-  // find center of mass
+  cohesionForce = Vector2f::normalized(cohesionForce);
 
   return cohesionForce;
 }
