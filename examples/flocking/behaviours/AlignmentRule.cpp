5788ca79b29de3230db5ae50f1ad3c0575022d2c
nate spielman <nate.spielman@mymail.champlain.edu>
2024-08-31 16:28:58 -0400

    Alignment rule implemented

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -7,6 +7,16 @@ Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
 
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
+  if (!neighborhood.empty()) {
+    Vector2f position = boid->transform.position;
+    float countFlockmates = 0;
+    for (int i = 0; i < neighborhood.size(); i++) {
+      Boid* neighbor = neighborhood[i];
+      averageVelocity += neighbor->velocity;
+      countFlockmates++;
+    }
+    averageVelocity /= countFlockmates;
+  }
 
   return Vector2f::normalized(averageVelocity);
 }
