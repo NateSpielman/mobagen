c22a3ad7cc1aa0e39cd3fe7f61e7c50482eacb59
nate spielman <nate.spielman@mymail.champlain.edu>
2024-08-31 15:30:23 -0400

    Separation Rule seems to be properly implemented and functioning

--- examples/flocking/behaviours/SeparationRule.cpp
+++ examples/flocking/behaviours/SeparationRule.cpp
@@ -6,15 +6,27 @@
 Vector2f SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   // Try to avoid boids too close
   Vector2f separatingForce = Vector2f::zero();
+  float desiredDistance = desiredMinimalDistance;
 
-  //    float desiredDistance = desiredMinimalDistance;
-  //
   //    // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
-  //    if (!neighborhood.empty()) {
-  //        Vector2f position = boid->transform.position;
-  //        int countCloseFlockmates = 0;
-  //        // todo: find and apply force only on the closest mates
-  //    }
+  if (!neighborhood.empty()) {
+    Vector2f position = boid->transform.position;
+    int countCloseFlockmates = 0;
+  //    // todo: find and apply force only on the closest mates
+    for (int i = 0; i < neighborhood.size(); i++) {
+      Boid* neighbor = neighborhood[i];
+      Vector2f neighborPosition = neighbor->transform.position;
+      float distance = Vector2f::Distance(position, neighborPosition);
+      if(distance < desiredDistance) {
+        //Apply the inverse proportion of the distance between to the repulsive force
+        Vector2f repulsiveForce = -(Vector2f::normalized(neighborPosition - position)/weight);
+        separatingForce = separatingForce + repulsiveForce;
+        countCloseFlockmates++;
+      }
+    }
+  }
+
+  //Clamp the maximum magnitude
 
   separatingForce = Vector2f::normalized(separatingForce);
 
