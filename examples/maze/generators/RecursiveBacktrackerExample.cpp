d12f7c70b2290fc503b720441d4ae8a86047f817
nate spielman <nate.spielman@mymail.champlain.edu>
2024-10-01 11:51:53 -0400

    Started work on recursive backtracker

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -34,6 +34,24 @@ std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const 
   std::vector<Point2D> visitables;
 
   // todo: implement this
+  //If we have not visited the left of the current point and it doesn't go out of bounds we add it to the stack
+  //When checking we only need to get the x position of the left point because it will have the same y as our current point
+  if(!visited[p.Left().x][p.y] && p.Left().x > -sideOver2-1) {
+    visitables.push_back(p.Left());
+  }
+  //We do a similar check for the right point, if we haven't visited it then push it to the stack
+  //Once again we only need to get the x position because it's on the same y
+  if(!visited[p.Right().x][p.y] && p.Right().x < sideOver2+1) {
+    visitables.push_back(p.Right());
+  }
+  //If we haven't visited the up point and it's in bounds we push it to the stack
+  if(!visited[p.x][p.Up().y] && p.Up().y > -sideOver2-1) {
+    visitables.push_back(p.Up());
+  }
+  //Lastly check the down point like we've been doing for all the other
+  if(!visited[p.x][p.Down().y] && p.Down().y < sideOver2+1) {
+    visitables.push_back(p.Down());
+  }
 
   return visitables;
 }
